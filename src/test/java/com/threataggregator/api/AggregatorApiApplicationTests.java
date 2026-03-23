7ce5e5dffa70baf2c3a1901b85917248f3af173b
Gerrard-Lewu <gerrardlewu@gmail.com>
2026-03-23 02:27:36 +0200

    Initial commit

---
+++ src/test/java/com/threataggregator/api/AggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threataggregator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
